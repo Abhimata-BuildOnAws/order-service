f25e042762803c3eeb6e0eb9ef8e60c463bb48db
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-23 20:22:14 +0800

    Fix create models

--- app/controllers/menu_item_controller.rb
+++ app/controllers/menu_item_controller.rb
@@ -1,6 +1,11 @@
 class MenuItemController < ApplicationController
   def create
-    menu_item = MenuItem.create(restaurant_id: params[:restaurant_id], name: params[:name], price: params[:price])
+    menu_item = MenuItem.create(
+      id: params[:id],
+      restaurant_id: params[:restaurant_id], 
+      name: params[:name], 
+      price: params[:price]
+    )
 
     render json: {menu_item: menu_item}, status: 200
   end
