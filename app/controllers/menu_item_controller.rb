09beee5758bcc3156481dec0618b01d7c3daf30b
Gary Peabody <aikarvin99@gmail.com>
2021-07-24 16:53:20 +0800

    Add ability to update restaurant and menu_item

--- app/controllers/menu_item_controller.rb
+++ app/controllers/menu_item_controller.rb
@@ -1,12 +1,20 @@
 class MenuItemController < ApplicationController
   def create
-    menu_item = MenuItem.create(
-      id: params[:id],
-      restaurant_id: params[:restaurant_id], 
-      name: params[:name], 
-      price: params[:price]
-    )
-
+    menu_item = MenuItem.find(params[:id])
+    if menu_item.exists?
+      menu_item.update(
+        restaurant_id: params[:restaurant_id], 
+        name: params[:name], 
+        price: params[:price]
+      )
+    else
+      menu_item = MenuItem.create(
+        id: params[:id],
+        restaurant_id: params[:restaurant_id], 
+        name: params[:name], 
+        price: params[:price]
+      )
+    end
     render json: {menu_item: menu_item}, status: 200
   end
 end
--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -1,16 +1,28 @@
 class RestaurantController < ApplicationController
   def create
-    restaurant = Restaurant.create(
-      id: params[:id],
-      name: params[:name],
-      street: params[:street],
-      stripe_id: params[:stripe_id],
-      state: params[:state],
-      country: params[:country],
-      latitude: params[:latitude],
-      longitude: params[:longitude]
-    )
-
+    restaurant = Restaurant.find(params[:id])
+    if restaurant.exists?
+      restaurant.update(
+        name: params[:name],
+        street: params[:street],
+        stripe_id: params[:stripe_id],
+        state: params[:state],
+        country: params[:country],
+        latitude: params[:latitude],
+        longitude: params[:longitude]
+      )
+    else
+      restaurant = Restaurant.create(
+        id: params[:id],
+        name: params[:name],
+        street: params[:street],
+        stripe_id: params[:stripe_id],
+        state: params[:state],
+        country: params[:country],
+        latitude: params[:latitude],
+        longitude: params[:longitude]
+      )
+    end
     render json: {restaurant: restaurant}, status: 200
   end
 end
