01b31ffcff6d4fa162f80e6f1a6b6d0204aadcf5
bingyuyap <bingyu.yap.21@gmail.com>
2021-07-25 02:20:56 +0800

    Update hitch serializer to use hitch object and coordinates param

--- app/serializers/hitch_serializer.rb
+++ app/serializers/hitch_serializer.rb
@@ -3,29 +3,27 @@
 class HitchSerializer
   include JSONAPI::Serializer
 
-  attributes :pickup, :latitude, :longitude, :total_pollution
+  attributes :pickup, :total_pollution
 
   # Returns a straight distance (not accounting route distance) from restaurant to the user in metres
   attribute :absolute_distance do |hitch, params|
-    current_user = params[:current_user]
-    hitch.distance_to(current_user.coordinates)
+    hitch.distance_to([params[:user_longitude], params[:user_longitude]])
   end
 
   # Returns the route distance from restaurant to the user in metres
   attribute :travel_distance do |hitch, params|
-    current_user = params[:current_user]
     @restaurant = hitch.restaurant
-    @distance = get_distance(@restaurant, hitch)
+    @distance = get_distance(hitch)
   end
 
   # Calculates effective pollution for each customer that hitches onto this delivery
   # In grams
-  attribute :effective_pollution do |hitch, _params|
+  attribute :effective_pollution do |hitch|
     @effective_pollution = hitch.each_pollution
   end
 
-  def self.get_distance(hitch, current_user)
-    distance = OpenRoutesService.get_travel_distance('driving-car', hitch.coordinates, current_user.coordinates)
+  def self.get_distance(obj)
+    obj.travel_distance
   end
 
   attribute :delivery_from do
@@ -35,7 +33,7 @@ class HitchSerializer
   # tree point = effective_pollution - 700
   # if effective_pollution > 700, minus points
   # if < 700, plus points
-  attribute :tree_point do
-    (@effective_pollution - 700) * -1
+  attribute :tree_point do |hitch|
+    (hitch.each_pollution - 700) * -1
   end
 end
